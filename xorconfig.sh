1c2c86ae89f7c958dbeed26e315010f0ab6c0a46
mysec000 <104643039+mysec000@users.noreply.github.com>
2022-04-29 23:23:14 +0530

    git hai

--- xorconfig.sh
+++ xorconfig.sh
@@ -3,7 +3,7 @@ gdbus monitor -y -d org.freedesktop.login1 |
     printf "$x\nReuqest Processed\n\n\n\n"
     case "$x" in
       *"{'LockedHint': <true>}"*) echo "Screen Locked";;
-      *"{'LockedHint': <false>}"*) kill -9 `cat train_pid.txt`;;
-      *"Unlock"*) kill -9 `cat train_pid.txt`;;
+      *"{'LockedHint': <false>}"*) bash common.sh;;
+      *"Unlock"*) bash common.sh;;
     esac
   done)
